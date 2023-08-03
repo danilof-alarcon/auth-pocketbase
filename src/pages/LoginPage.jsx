b3922faa500178e0c3430657878ff46ed7b8dd57
Danilo <danilof.alarcon@gmail.com>
2023-08-03 15:59:26 -0500

    Login, Register and NotFoundPage Design

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { useData } from "../context/DataContextProvider"
-import { useEffect } from "react";
+import { useEffect, useState } from "react"
+import { Box, Button, Card, Container, FormControl, Grid, Paper, TextField, Typography } from "@mui/material"
+import { Formik, Form, Field } from 'formik'
+import Logo from "../assets/logoipsum-288.svg"
+import LoadingButton from '@mui/lab/LoadingButton';
 
 
 function Login() {
@@ -22,8 +26,78 @@ function Login() {
 
     // Code
 
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const initialValues = {
+        email: '',
+        password: '',
+    };
+
+    const handleSubmit = async(values) => {
+        setIsSubmitting(true)
+        const { email, password } = values
+        console.log(email, password);
+    }
+
     return(
-        <h1>Login</h1>
+        <Box sx={{ backgroundColor: "#dfe4ea" }} >
+            <Container fixed >
+                <Grid 
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                paddingY={5}
+                minHeight="100vh" >
+                    <img src={Logo} width="250" alt="Logo" className="logo" />
+                    <Card sx={{ 
+                        width: {md: 500, xs: "100%"},
+                        boxShadow: "none",
+                        borderRadius: 4,
+                        marginY: 5
+                    }} >
+                        <Paper sx={{ padding: {md: 5, xs: 4} }}>
+                            <Typography variant="h1" fontSize={24} fontWeight={600} textAlign={"center"} >Login</Typography>
+                            <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                                <Form>
+
+                                    <FormControl fullWidth margin="normal" variant="standard">
+                                        <Field as={TextField} type="email" name="email" label="Email" variant="standard" autoComplete="on" fullWidth required />
+                                    </FormControl>
+
+                                    <FormControl fullWidth margin="normal" variant="standard">
+                                        <Field as={TextField} type="password" name="password" label="Password" variant="standard" autoComplete="on" fullWidth required />
+                                    </FormControl>
+
+                                    <LoadingButton
+                                    fullWidth
+                                    type="submit"
+                                    variant="contained"
+                                    size="large"
+                                    color="primary"
+                                    loading={isSubmitting}
+                                    disabled={isSubmitting}
+                                    sx={{
+                                        boxShadow: "none",
+                                        fontWeight: 800,
+                                        paddingY: 2,
+                                        borderRadius: 2,
+                                        marginTop: 2,
+                                    }}
+                                    >
+                                        <span>Login</span>
+                                    </LoadingButton>
+                                    
+                                </Form>
+                            </Formik>
+                        </Paper>
+                    </Card>
+
+                    <Button variant="outlined" sx={{ borderRadius: 2}} onClick={() => navigate("/register")}>Register</Button>
+
+                </Grid>
+            </Container>
+        </Box>
     )
 }
 
--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,6 +1,38 @@
+import { Box, Button, Card, Container, Grid, Paper, Typography } from "@mui/material"
+import Logo from "../assets/logoipsum-288.svg"
+import { useNavigate } from "react-router-dom";
+
 function NotFound() {
+
+    const navigate = useNavigate();
+
     return(
-        <h1>NotFound</h1>
+        <Box sx={{ backgroundColor: "#dfe4ea" }} >
+            <Container fixed >
+                <Grid 
+                container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+                paddingY={5}
+                minHeight="100vh" >
+                    <img src={Logo} width="250" alt="Logo" className="logo" />
+                    <Card sx={{ 
+                        width: {md: 500, xs: "100%"},
+                        boxShadow: "none",
+                        borderRadius: 4,
+                        marginY: 5
+                    }} >
+                        <Paper sx={{ padding: {md: 5, xs: 4} }}>
+                            <Typography variant="h1" fontSize={32} fontWeight={600} textAlign={"center"} >Not Found Page</Typography>
+                        </Paper>
+                    </Card>
+
+                    <Button variant="outlined" sx={{ borderRadius: 2}} onClick={() => navigate("/")}>Back to Home</Button>
+
+                </Grid>
+            </Container>
+        </Box>
     )
 }
 

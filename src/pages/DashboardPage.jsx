29eddff3eca81bda4b2771fb20acffb09510e665
Danilo <danilof.alarcon@gmail.com>
2023-08-03 12:58:44 -0500

    Auth Rule for each page

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,4 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useData } from "../context/DataContextProvider"
+import { useEffect } from "react";
+
+
 function Dashboard() {
+
+    // Auth
+
+    const { auth } = useData()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!auth) {
+            navigate("/");
+        }
+    }, [auth, navigate]);
+
+    if (!auth) {
+        return null;
+    }
+
+    // Code
+
     return(
         <h1>Dashboard</h1>
     )
--- src/pages/ForgotPasswordPage.jsx
+++ src/pages/ForgotPasswordPage.jsx
@@ -1,4 +1,27 @@
+import { useNavigate } from "react-router-dom";
+import { useData } from "../context/DataContextProvider"
+import { useEffect } from "react";
+
+
 function ForgotPassword() {
+
+    // Auth
+
+    const { auth } = useData()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (auth) {
+            navigate("/dashboard");
+        }
+    }, [auth, navigate]);
+
+    if (auth) {
+        return null;
+    }
+
+    // Code
+
     return(
         <h1>ForgotPassword</h1>
     )
